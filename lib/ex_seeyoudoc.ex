2f1836dc0bda4931f843490c25c60fb91947b51d
Noel Del Castillo <noel@pragtechnologies.com>
2024-01-30 10:00:44 +0800

    Initial commit

---
+++ lib/ex_seeyoudoc.ex
@@ -0,0 +1,18 @@
+defmodule ExSeeyoudoc do
+  @moduledoc """
+  Documentation for `ExSeeyoudoc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExSeeyoudoc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_seeyoudoc_test.exs
@@ -0,0 +1,8 @@
+defmodule ExSeeyoudocTest do
+  use ExUnit.Case
+  doctest ExSeeyoudoc
+
+  test "greets the world" do
+    assert ExSeeyoudoc.hello() == :world
+  end
+end
